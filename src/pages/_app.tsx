5bd696a6395788d6050bb12f1f93860f8c17a8cf
Joshua Sy <z5259375@unsw.edu.au>
2025-02-21 21:14:32 +1100

    auth setupt

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,15 +1,29 @@
 import { GeistSans } from "geist/font/sans";
 import { type AppType } from "next/app";
-
+import {
+  ClerkProvider,
+  SignInButton,
+  SignedIn,
+  SignedOut,
+  UserButton
+} from '@clerk/nextjs'
 import { api } from "~/utils/api";
 
 import "~/styles/globals.css";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
   return (
-    <div className={GeistSans.className}>
-      <Component {...pageProps} />
-    </div>
+    <ClerkProvider>
+      <SignedOut>
+        <SignInButton />
+      </SignedOut>
+      <SignedIn>
+        <UserButton />
+      </SignedIn>
+      <div className={GeistSans.className}>
+        <Component {...pageProps} />
+      </div>
+    </ClerkProvider>
   );
 };
 
